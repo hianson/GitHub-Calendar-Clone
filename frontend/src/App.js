2fdc24434fee89f83d22dfbae1497aa779af35e2
Anson <leungans@gmail.com>
2017-11-07 18:03:53 -0800

    light styling, remove innecessary class attributes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -25,7 +24,7 @@ class App extends Component {
           >
             <defs>
                 <g id="day">
-                    <rect class="day" width="10" height="10" fill="#ebedf0" />
+                    <rect width="10" height="10" fill="#ebedf0" />
                 </g>
             </defs>
             <g transform="translate(16, 20)">
@@ -141,34 +140,31 @@ class App extends Component {
               <g transform="translate(663, 0)">
               </g>
               <g transform="translate(676, 0)">
-                  <rect class="day" width="10" height="10" x="-39" y="0" fill="#c6e48b" data-count="1" data-date="2017-11-05"/>
-                  <rect class="day" width="10" height="10" x="-39" y="12" fill="#196127" data-count="12" data-date="2017-11-06"/>
+                  <rect width="10" height="10" x="-39" y="0" fill="#c6e48b" data-count="1" data-date="2017-11-05"/>
+                  <rect width="10" height="10" x="-39" y="12" fill="#196127" data-count="12" data-date="2017-11-06"/>
               </g>
 
-              <text x="13" y="-10" class="month">Nov</text>
-              <text x="61" y="-10" class="month">Dec</text>
-              <text x="109" y="-10" class="month">Jan</text>
-              <text x="169" y="-10" class="month">Feb</text>
-              <text x="217" y="-10" class="month">Mar</text>
-              <text x="265" y="-10" class="month">Apr</text>
-              <text x="325" y="-10" class="month">May</text>
-              <text x="373" y="-10" class="month">Jun</text>
-              <text x="421" y="-10" class="month">Jul</text>
-              <text x="481" y="-10" class="month">Aug</text>
-              <text x="529" y="-10" class="month">Sep</text>
-              <text x="577" y="-10" class="month">Oct</text>
+              <text x="13" y="-10" className="graph-text">Nov</text>
+              <text x="61" y="-10" className="graph-text">Dec</text>
+              <text x="109" y="-10" className="graph-text">Jan</text>
+              <text x="169" y="-10" className="graph-text">Feb</text>
+              <text x="217" y="-10" className="graph-text">Mar</text>
+              <text x="265" y="-10" className="graph-text">Apr</text>
+              <text x="325" y="-10" className="graph-text">May</text>
+              <text x="373" y="-10" className="graph-text">Jun</text>
+              <text x="421" y="-10" className="graph-text">Jul</text>
+              <text x="481" y="-10" className="graph-text">Aug</text>
+              <text x="529" y="-10" className="graph-text">Sep</text>
+              <text x="577" y="-10" className="graph-text">Oct</text>
 
 
-
-
-
-              <text text-anchor="start" class="wday" dx="-14" dy="8" style={{ display:"none" }}>Sun</text>
-              <text text-anchor="start" class="wday" dx="-14" dy="20">Mon</text>
-              <text text-anchor="start" class="wday" dx="-14" dy="32" style={{ display:"none" }}>Tue</text>
-              <text text-anchor="start" class="wday" dx="-14" dy="44">Wed</text>
-              <text text-anchor="start" class="wday" dx="-14" dy="57" style={{ display:"none" }}>Thu</text>
-              <text text-anchor="start" class="wday" dx="-14" dy="69">Fri</text>
-              <text text-anchor="start" class="wday" dx="-14" dy="81" style={{ display:"none" }}>Sat</text>
+              <text className="graph-text" dx="-14" dy="8" style={{ display:"none" }}>Sun</text>
+              <text className="graph-text" dx="-14" dy="20">Mon</text>
+              <text className="graph-text" dx="-14" dy="32" style={{ display:"none" }}>Tue</text>
+              <text className="graph-text" dx="-14" dy="44">Wed</text>
+              <text className="graph-text" dx="-14" dy="57" style={{ display:"none" }}>Thu</text>
+              <text className="graph-text" dx="-14" dy="69">Fri</text>
+              <text className="graph-text" dx="-14" dy="81" style={{ display:"none" }}>Sat</text>
 
             </g>
           </svg>
