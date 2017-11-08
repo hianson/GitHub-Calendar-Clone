e4c588b58bc64f1b3f95086628144eb79e143fb9
Anson <leungans@gmail.com>
2017-11-07 18:54:17 -0800

    create PracticeSession model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,20 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107042623) do
+ActiveRecord::Schema.define(version: 20171107030450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
